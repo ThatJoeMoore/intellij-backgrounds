dad0e7d15d657058157e4382ef817c560005a178
Joseph Moore <yo.yo.yossarian@gmail.com>
2019-03-26 22:37:46 -0600

    add script and images

--- generate.sh
+++ generate.sh
@@ -3,5 +3,7 @@
 color=$1
 file=$2
 
-magick -size 4000x50 xc:#$1 $2.png
+#magick -size 4000x50 xc:#$1 $2.png
+docker run -v $PWD:/imgs dpokidov/imagemagick -size 4000x50 xc:#$color /imgs/$2.png
+git add $2.png
 
